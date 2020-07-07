30f1edb9cf1ddd86a80c4008787754cf82079e37
kouml <key.mtrgtring@gmail.com>
2020-07-07 20:43:37 +0900

    add tray feature and toggle/quit menu

---
+++ README.md
@@ -0,0 +1,24 @@
+# Simple auto formatting tool for DeepL Translator: DeepLHack
+![guenv demo](demo.gif)
+This tool can fix this type of paste problem.
+DeepLHack is the simplest auto-formatting tool to use DeepL Translator.
+
+- Available in Several Platform(Windows, MacOS, Linux)
+- No need to modify the clipboard to formatting. Just type Cmd(Ctrl)+Shift+V and reformat.
+
+- You can mange git user env easily.
+
+# Installation
+## For Mac
+download latest tools from [release pages](https://github.com/kouml/DeepLHack/releases)
+
+## For Windows
+download latest tools from [release pages](https://github.com/kouml/DeepLHack/releases)
+
+## For Linux
+download latest tools from [release pages](https://github.com/kouml/DeepLHack/releases)
+
+# Usage
+
+## Cmd(Ctrl)+Shift+V
+- remove redundant spaces from clipboard
--- main.js
+++ main.js
@@ -3,14 +3,73 @@ const {
   dialog,
   globalShortcut,
   clipboard,
+  Menu,
+  Tray,
   Notification
 } = require('electron')
 
 const pkg = require('./package.json');
 const cmd = 'CommandOrControl+shift+V';
+let appIcon = null;
+let toggled = true;
 
 app.whenReady().then(() => {
-  //二重起動の防止
+  const ret = globalShortcut.register(cmd, () => {
+    // console.log("ok");
+    let str = clipboard.readText();
+    str = str.replace(/\s/g, ' ');
+    // https: //github.com/electron/electron/issues/10864
+    var myNotification = new Notification({
+      title: 'DeepLHacked',
+      body: 'remove redundant spaces from clipboard'
+    })
+    myNotification.show();
+    clipboard.writeText(str, 'selection');
+  });
+
+
+  // appIcon = new Tray('23252616.jpeg');
+  appIcon = new Tray('icon/com_93620.png');
+  const contextMenu = Menu.buildFromTemplate([{
+      label: 'Toggle',
+      type: 'checkbox',
+      click() {
+        if (toggled) {
+          globalShortcut.unregisterAll();
+          toggled = false;
+        } else {
+          // console.log("ret");
+          const ret = globalShortcut.register(cmd, () => {
+            // console.log("ok");
+            let str = clipboard.readText();
+            str = str.replace(/\s/g, ' ');
+            // https: //github.com/electron/electron/issues/10864
+            var myNotification = new Notification({
+              title: 'DeepLHacked',
+              body: 'remove redundant spaces from clipboard'
+            })
+            myNotification.show();
+            clipboard.writeText(str, 'selection');
+          });
+          ret;
+        }
+      }
+    },
+    {
+      label: 'Quit',
+      type: 'normal',
+      click() {
+        app.quit()
+      }
+    }
+  ]);
+
+  console.log("first");
+  contextMenu.items[0].checked = true;
+  appIcon.setContextMenu(contextMenu);
+  console.log("end");
+
+  // prevent from double exectuion
   const doubleboot = app.requestSingleInstanceLock();
   if (!doubleboot) {
     app.quit();
@@ -23,17 +82,6 @@ app.whenReady().then(() => {
     detail: 'It may conflict with other keyboard shortcut key.'
   };
 
-  const ret = globalShortcut.register(cmd, () => {
-    let str = clipboard.readText();
-    str = str.replace(/\s/g, ' ');
-    // https: //github.com/electron/electron/issues/10864
-    var myNotification = new Notification({
-      title: 'DeepLHacked',
-      body: 'remove redundant spaces from clipboard'
-    })
-    myNotification.show();
-    clipboard.writeText(str, 'selection');
-  })
 
   if (!ret) {
     const response = dialog.showMessageBox(options);
